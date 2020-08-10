088e144e96504f7980a70cb1963a53ad2fa88fc3
Tran Duc Nguyen <1512227@hcmut.edu.vn>
2020-08-10 11:24:17 +0700

    open

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,5 @@ Rails.application.routes.draw do
   delete  "logout"   => "session#destroy"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :users
+  root to: 'session#new' 
 end
